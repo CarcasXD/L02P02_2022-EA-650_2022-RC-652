889505feb340678e10970541fc9711d915d9b8fe
CarcasXD <carlossalesianoprro@gmail.com>
2025-04-03 17:49:34 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2022-EA-650_2022-RC-652/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022_EA_650_2022_RC_652.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
