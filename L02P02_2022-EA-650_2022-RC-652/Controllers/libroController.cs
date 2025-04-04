ea0d1f4c75e0b11d7f675e61adef29a96166b899
alex <ee653522@gmail.com>
2025-04-03 19:53:28 -0600

    xd

--- L02P02_2022-EA-650_2022-RC-652/Controllers/libroController.cs
+++ L02P02_2022-EA-650_2022-RC-652/Controllers/libroController.cs
@@ -130,7 +130,7 @@ namespace L02P02_2022_EA_650_2022_RC_652.Controllers
                             }).ToList();
 
             int cantidadTotal = detalles.Count;
-            double totalVenta = detalles.Sum(d => d.Subtotal);
+            double totalVenta = detalles.Sum(d => d.Subtotal) ?? 0;
 
             pedido.cantidad_libro = cantidadTotal;
             pedido.total = totalVenta;
