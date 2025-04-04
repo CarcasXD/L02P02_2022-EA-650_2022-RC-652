b4a0a842b97cf2ff5ba3b1d0b383f24e1ba4c564
alex <ee653522@gmail.com>
2025-04-03 20:37:56 -0600

    xdd

--- L02P02_2022-EA-650_2022-RC-652/Models/libroContext.cs
+++ L02P02_2022-EA-650_2022-RC-652/Models/libroContext.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
-namespace L02P02_2022_EA_650_2022_RC_652.Models
 
+namespace L02P02_2022_EA_650_2022_RC_652.Models
 {
     public class libroContext : DbContext
     {
-        public libroContext(DbContextOptions options) : base(options) 
-        {
+        public libroContext(DbContextOptions options) : base(options)
+        { }
 
-        }
         public DbSet<autores> autores { get; set; }
         public DbSet<categorias> categorias { get; set; }
         public DbSet<clientes> clientes { get; set; }
